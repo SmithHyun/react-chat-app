12f9a9d9064702ef6a2af54da81f6f1d939181be
jshyun91Cloud <jshyun91@gmail.com>
2020-06-18 18:26:40 +0200

    feat: MongoDB connection and user registration function development (backend)
    Install MongoDB
    Install cors
    Develop User registration function
    Set config file.

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
+
+
+var mongo = require('mongodb');
+
+const config = process.env.NODE_ENV === "prod"
+    ? require("./application.prod.json") : require("./application.dev.json");
+const url = config.db.url;
+const dbName = config.db.dbName;
+mongo.connect(url, { useUnifiedTopology: true, useNewUrlParser: true, })
+    .catch(err => console.error(err.stack))
+    .then(db => {
+        console.log('mongodb connection success(app.js)');
+        app.locals.db = db.db(dbName);
+    });
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,19 +40,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,54 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+
+/* Sign In */
+/* x-www-form-urlencoded */
+router.post('/', function (req, res, next) {
+
+    let userId = req.body.userId;
+    console.log("userId", userId);
+
+    if (userId === null) {
+        console.log("Input required");
+        var data = {
+            "result": false,
+            "message": "Fields are required.!",
+            "errors": null,
+            "data": null
+        };
+        res.send(data);
+    }
+
+    const db = req.app.locals.db;
+    const usersCollection = db.collection('users');
+    usersCollection.insertOne({
+        userId: userId,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }, function (error, result) {
+        if (error) {
+            var data = {
+                "result": false,
+                "message": null,
+                "errors": error,
+                "data": null
+            };
+            res.send(data);
+        } else {
+            var data = {
+                "result": true,
+                "message": null,
+                "errors": null,
+                "data": result
+            };
+            res.send(data);
+        }
+    });
 });
 
 module.exports = router;
